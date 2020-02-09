8d30a75d43c8bd21c02b0cf0b3db5591531bc50c
glyzinieh <50325323+glyzinieh@users.noreply.github.com>
2020-02-09 17:26:45 +0900

    app.pyを修正
    jsonを辞書型に変換するように修正

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask,request
+import ast
 import json
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def hello():
     data = request.get_json()
-    type = data['type']
-    return type
+    data = ast.literal_eval(data)
+    t = data["type"]
+    print(t)
+    return t
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
