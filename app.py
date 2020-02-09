962ee207c0695d62ab7145a29b4ae024327201f9
glyzinieh <50325323+glyzinieh@users.noreply.github.com>
2020-02-09 13:02:16 +0900

    heroku対応
    herokuで動作するように変更

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from flask import Flask
 import json
 
-#データ取得
-param = json.loads(request.json)
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    param = json.loads(request.json)
+    type = param.get('type')
+    print(type)
+    return type
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
