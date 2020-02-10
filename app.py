7fa8d9b1eeddab2de1e844864bc17329191516b0
glyzinieh <50325323+glyzinieh@users.noreply.github.com>
2020-02-10 22:33:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,9 @@ app = Flask(__name__)
 def hello():
     data = request.data.decode()
     data = ast.literal_eval(data)
-    #TYPE = data["type"]
-    #print(TYPE)
-    #return TYPE
-    print(data)
-    print(type(data))
-    return 'OK'
+    TYPE = data["type"]
+    print(TYPE)
+    return TYPE
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
